61cb68abf0580ecbf0c0fc74571f1cde0034d7d8
demdeni <demdeni@gmail.com>
2018-04-03 13:21:01 +0300

    23423423423

---
+++ NunitTests/Test1.cs
@@ -0,0 +1,26 @@
+﻿using ConsoleAppCoreTool;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NunitTests
+{
+
+    public class Test1
+    {
+        [Test]
+        public static void TestMethod1()
+        {
+            if (Program.Calculation(10, 5) == 15)
+            {
+                //hkjh
+            }
+            else
+            {
+                throw new Exception("test failed");
+            }
+
+        }
+    }
+}
---
+++ NunitTests/TestBase.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace NunitTests
+{
+    [TestFixture]
+    public class TestBase
+    {
+
+        [SetUp]
+        public void TestInitialize()
+        {
+            
+
+        }
+
+
+    }
+}
