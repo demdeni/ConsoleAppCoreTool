07716989ebe98eb5e31f2fc1c945117195cb853e
demdeni <demdeni@gmail.com>
2018-04-09 16:29:34 +0300

    dfghdhg

--- ConsoleAppCoreTool/Program.cs
+++ ConsoleAppCoreTool/Program.cs
@@ -9,10 +9,7 @@ namespace ConsoleAppCoreTool
 
         public static int Calculation(int _num1, int _num2)
         {
-            var num1 = _num1;
-            var num2 = _num1;
-
-            return num1 + num2;
+            return _num1 + _num2;
         }
     }
 }
